f0e66a0333b65f8251557a2794aae6d41345f2af
SC1709 <sachinchoudhary97599@gmail.com>
2024-09-11 19:09:47 +0530

    third day

---
+++ day3.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+//using xor
+int Duplicates1(vector<int> arr,int n){
+    int xor1=0,xor2=0; 
+    for(int i=0;i<n;i++){ 
+        xor2=xor2 ^ arr[i];
+        xor1=xor1 ^ (i+1);
+    }
+    xor2=xor2 ^ arr[n];
+    return xor1 ^ xor2;
+}
+int Duplicates2(vector<int> arr,int n){
+    int total = (n*(n+1))/2;
+    int sum=0;
+    for(int i=0;i<=n;i++){ //1 missing number so
+        sum+=arr[i];
+    }
+    return sum-total;
+}
+int main(){
+    vector<int> a={1,1};
+    // sort(a.begin(),a.end());
+    int n = a.size();
+    cout<<Duplicates2(a,n-1)<<endl;
+    cout<<Duplicates2(a,n-1);
+    return 0;
+}
