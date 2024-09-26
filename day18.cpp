09764f7ab3e5343685b8f0e13a9dabc2066b7573
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2024-09-26 18:24:02 +0530

    day 18

---
+++ day18.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+//using loop
+int PrintDivisors(int n){
+    int count=0; //store divisors count
+    for(int i=1;i*i<=n;i++){ //loop till sqrt of n as gets till sqrt(n) all divisors
+        if(n%i==0){
+            count++;
+            if(n/i != i){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+    int n=997;
+    cout<<PrintDivisors(n);
+    return 0; 
+}
